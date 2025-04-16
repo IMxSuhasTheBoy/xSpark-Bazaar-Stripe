9e32ca232f352be2731f6e41547cab55c9d3b499
IMxSuhasTheBoy <dannylivecsr2@gmail.com>
2025-04-16 19:35:03 +0530

    02: customizations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,32 @@
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
+
 export default function Home() {
   return (
-    <div className="flex h-screen items-center justify-center">
-      <p>xSpark-Bazaar</p>
+    <div className="p-6">
+      <div className="flex h-screen flex-col gap-y-4">
+        <div>
+          <Button variant="elevated">ss</Button>
+        </div>
+        <div>
+          <Input placeholder="ssssss" />
+        </div>
+        <div>
+          <p>xSpark-Bazaar testing components customization</p>
+        </div>
+        <div>
+          <Progress value={50} />
+        </div>
+        <div>
+          <Textarea placeholder="Textarea" />
+        </div>
+        <div>
+          <Checkbox />
+        </div>
+      </div>
     </div>
   );
 }
